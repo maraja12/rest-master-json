b3b67e7cd0a733228c1b7d52c81675194b2d1cdb
maraja12 <marijarajic64@gmail.com>
2024-12-15 14:18:04 +0100

    Deleted target files

--- target/classes/liquibase/sqlserver/setup/ddl/create-profession.sql
+++
@@ -1,5 +0,0 @@
-create table profession (
-    id bigint identity not null,
-    name varchar(30) not null unique,
-    primary key (id)
-)
--- target/classes/liquibase/sqlserver/setup/ddl/create-project.sql
+++
@@ -1,7 +0,0 @@
-create table project(
-       id bigint identity not null,
-       name varchar(30) not null,
-       budget decimal(18,2) not null,
-       constraint ck_budget_limit check (budget > 0),
-       primary key (id)
-)
